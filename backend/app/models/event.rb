904a54d9867926d9673ef7a553c30190ce1ced02
youichiro <cinnamon416@gmail.com>
2020-08-17 00:23:35 +0900

    [fix] typo

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, presence: true, length: { maximux: 100 }
+  validates :name, presence: true, length: { maximum: 100 }
   validates :start, presence: true
   validates :end, presence: true
 end
