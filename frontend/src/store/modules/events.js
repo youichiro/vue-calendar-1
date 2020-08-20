e3f1039b17f619e5563dabb381d896d6ae1d2f2a
youichiro <cinnamon416@gmail.com>
2020-08-20 21:52:11 +0900

    deleteEventアクションを追加

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -20,6 +20,7 @@ const getters = {
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  removeEvent: (state, event) => (state.events = state.events.filter(e => e.id !== event.id)),
   setEvent: (state, event) => (state.event = event),
   resetEvent: state => (state.event = null)
 };
@@ -28,6 +29,11 @@ const actions = {
   async fetchEvents({ commit }) {
     const response = await axios.get('http://localhost:3000/events');
     commit('setEvents', response.data);
+  },
+  async deleteEvent({ commit }, id) {
+    const response = await axios.delete(`http://localhost:3000/events/${id}`);
+    commit('removeEvent', response.data);
+    commit('resetEvent');
   },
   setEvent({ commit }, event) {
     commit('setEvent', event);
