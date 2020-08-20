41115c8403ffb13e0699c19b9631c0b7f65572c8
youichiro <cinnamon416@gmail.com>
2020-08-20 20:53:38 +0900

    [add] 詳細画面を作成

---
+++ frontend/src/components/Detail.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card light>
+    <v-card-actions class="d-flex justify-end pa-2">
+      <v-btn icon @click="close">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-title>
+      {{ event.name }}
+    </v-card-title>
+    <v-card-text>
+      {{ event.start }}
+      {{ event.end }}
+    </v-card-text>
+    <v-card-text>
+      {{ event.description }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Detail',
+  computed: {
+    ...mapGetters('events', ['event'])
+  },
+  methods: {
+    ...mapActions('events', ['resetEvent']),
+    close() {
+      this.resetEvent();
+    }
+  }
+};
+</script>
