97e33c797c6bd3333f880e3f5d34581981b2795e
youichiro <cinnamon416@gmail.com>
2020-08-21 21:23:04 +0900

    FormDialog.vueを作成

---
+++ frontend/src/components/FormDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card light width="600">
+    <v-card-actions class="d-flex justify-end pa-2">
+      <v-btn icon @click="close">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-text>
+      ああ
+    </v-card-text>
+    <v-card-actions class="d-flex justify-end">
+      <v-btn @click="cancel">キャンセル</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate';
+import { required } from 'vuelidate/lib/validators';
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'FormDialog',
+  mixins: [validationMixin],
+  validations: {
+    title: { required }
+  },
+  data: () => ({
+    title: ''
+  }),
+  computed: {
+    ...mapGetters('events', ['event'])
+  },
+  created() {
+    this.title = this.event.title;
+  },
+  methods: {
+    ...mapActions('events', ['resetEvent', 'setEditMode']),
+    close() {
+      this.resetEvent();
+      this.setEditMode(false);
+    },
+    cancel() {
+      this.setEditMode(false);
+    }
+  }
+};
+</script>
