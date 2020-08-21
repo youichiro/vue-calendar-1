de8d3f40f61f57d4629b6728dcff322defed1300
youichiro <cinnamon416@gmail.com>
2020-08-22 00:17:37 +0900

    updateEventアクションを追加

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -23,6 +23,8 @@ const getters = {
 const mutations = {
   setEvents: (state, events) => (state.events = events),
   removeEvent: (state, event) => (state.events = state.events.filter(e => e.id !== event.id)),
+  updateEvent: (state, event) =>
+    (state.events = state.events.map(e => (e.id === event.id ? event : e))),
   setEvent: (state, event) => (state.event = event),
   resetEvent: state => (state.event = null),
   setEditMode: (state, bool) => (state.isEditMode = bool)
@@ -37,6 +39,10 @@ const actions = {
     const response = await axios.delete(`http://localhost:3000/events/${id}`);
     commit('removeEvent', response.data);
     commit('resetEvent');
+  },
+  async updateEvent({ commit }, event) {
+    const response = await axios.put(`http://localhost:3000/events/${event.id}`, event);
+    commit('updateEvent', response.data);
   },
   setEvent({ commit }, event) {
     commit('setEvent', event);
