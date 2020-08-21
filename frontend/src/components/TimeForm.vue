764a2f783b30e5de9a24caf2af747ba057818b9b
youichiro <cinnamon416@gmail.com>
2020-08-21 22:04:55 +0900

    TimeForm.vueを作成

---
+++ frontend/src/components/TimeForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-text-field type="time" v-model="time" :label="value.label"></v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'TimeForm',
+  props: ['value'],
+  computed: {
+    time: {
+      get() {
+        return this.value.value;
+      },
+      set(value) {
+        this.$emit('input', { ...this.value, value });
+      }
+    }
+  }
+};
+</script>
