de808d0392d0859b630a2c5cb817254fa7ca1005
youichiro <cinnamon416@gmail.com>
2020-08-22 15:28:38 +0900

    serializerを指定

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -1,16 +1,16 @@
 class EventsController < ApplicationController
   def index
-    render json: Event.all
+    render json: Event.all, each_serializer: EventSerializer
   end
 
   def show
-    render json: Event.find(params[:id])
+    render json: Event.find(params[:id]), serializer: EventSerializer
   end
 
   def create
     event = Event.new(event_params)
     if event.save
-      render json: event
+      render json: event, serializer: EventSerializer
     else
       render json: event.errors, status: 422
     end
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
   def update
     event = Event.find(params[:id])
     if event.update(event_params)
-      render json: event
+      render json: event, serializer: EventSerializer
     else
       render json: event.errors, status: 422
     end
@@ -28,7 +28,7 @@ class EventsController < ApplicationController
   def destroy
     event = Event.find(params[:id])
     event.destroy!
-    render json: event
+    render json: event, serializer: EventSerializer
   end
 
   private
