1e9da3f3e59c76eb7103c8d6c64260263c0e2c76
youichiro <cinnamon416@gmail.com>
2020-08-30 23:51:48 +0900

    詳細画面の見た目を編集

--- frontend/src/components/DetailDialog.vue
+++ frontend/src/components/DetailDialog.vue
@@ -12,25 +12,49 @@
       </v-btn>
     </v-card-actions>
     <v-card-title>
-      {{ event.name }}
+      <v-row>
+        <v-col cols="2" align-self="center" class="d-flex justify-center">
+          <font-awesome-icon icon="square" :color="event.color" style="font-size: 20px;" />
+        </v-col>
+        <v-col>
+          {{ event.name }}
+        </v-col>
+      </v-row>
     </v-card-title>
     <v-card-text>
-      {{ event.start }}
-      {{ event.end }}
+      <v-row>
+        <v-col cols="2" align-self="center" class="d-flex justify-center">
+          <font-awesome-icon icon="calendar-day" style="font-size: 20px;" />
+        </v-col>
+        <v-col>
+          {{ dateText }}
+        </v-col>
+      </v-row>
     </v-card-text>
     <v-card-text>
-      {{ event.description }}
+      <v-row>
+        <v-col cols="2" align-self="center" class="d-flex justify-center">
+          <font-awesome-icon icon="align-left" style="font-size: 20px;" />
+        </v-col>
+        <v-col>
+          {{ event.description || 'no description' }}
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import { eventDateFormatter } from '../functions/formatters';
 
 export default {
   name: 'Detail',
   computed: {
-    ...mapGetters('events', ['event'])
+    ...mapGetters('events', ['event']),
+    dateText() {
+      return eventDateFormatter(this.event);
+    }
   },
   methods: {
     ...mapActions('events', ['resetEvent', 'deleteEvent', 'setEditMode']),
