8c1cfbedda5de86e54420d54118ae0cf275c1101
youichiro <cinnamon416@gmail.com>
2020-09-06 12:34:00 +0900

    予定一覧の順番を変更

--- frontend/src/functions/filters.js
+++ frontend/src/functions/filters.js
@@ -7,5 +7,10 @@ export const filterEventsByDate = (events, date) => {
     .filter(
       event =>
         event.startDate === date || event.endDate === date || moment(date).isBetween(event.startDate, event.endDate)
-    );
+    )
+    .sort((a, b) => {
+      if (a.start < b.start) return -1;
+      if (a.start > b.start) return 1;
+      return 0;
+    });
 };
