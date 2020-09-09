c3b862bf78e92d9102f22ec52010d4334c6e1636
youichiro <cinnamon416@gmail.com>
2020-09-09 23:26:14 +0900

    カレンダー一覧を表示

--- frontend/src/components/CalendarList.vue
+++ frontend/src/components/CalendarList.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    {{ calendars }}
-  </div>
+  <v-list dense>
+    <v-list-item v-for="(calendar, i) in calendars" :key="i">
+      <v-list-item-icon>
+        <font-awesome-icon icon="square" :color="calendar.color" style="font-size: 20px" />
+      </v-list-item-icon>
+      <v-list-item-content>
+        {{ calendar.name }}
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
