21864586065bd571e7323e932b40f7763cbf30f6
youichiro <cinnamon416@gmail.com>
2020-09-11 23:37:32 +0900

    calendar_idをparamsで受け取る

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -34,6 +34,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color, :calendar_id)
   end
 end
