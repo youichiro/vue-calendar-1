7e263a2e935114ea66bc67b24ce81dc1c59b8ac6
youichiro <cinnamon416@gmail.com>
2020-09-11 20:51:26 +0900

    fontawesomeの設定を変更

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,14 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 Vue.config.productionTip = false;
-
-library.add(fas);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   store,
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,6 +1,14 @@
+import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: 'fa',
+    values: {
+      cancel: 'fas fa-ban',
+    }
+  }
+});
