6e9f3a2a28a7a2e34276281f3019c56c3c76578b
youichiro <cinnamon416@gmail.com>
2020-09-11 22:19:36 +0900

    fix

--- frontend/src/components/DialogSection.vue
+++ frontend/src/components/DialogSection.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col cols="2" align-self="center" class="d-flex justify-center">
-      <v-icon :color="color" size="20px">${{ icon }}</v-icon>
+      <v-icon :color="color" size="20px" v-if="icon">${{ icon }}</v-icon>
     </v-col>
     <v-col class="d-flex align-center">
       <slot></slot>
