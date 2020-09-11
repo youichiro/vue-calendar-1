39e58d9c1266b9d3079237051f7a5c50cbf001a3
youichiro <cinnamon416@gmail.com>
2020-09-11 21:32:53 +0900

    カレンダーリストをチェックリストに

--- frontend/src/components/CalendarList.vue
+++ frontend/src/components/CalendarList.vue
@@ -1,11 +1,8 @@
 <template>
   <v-list dense>
     <v-list-item v-for="(calendar, i) in calendars" :key="i">
-      <v-list-item-icon>
-        <v-icon :color="calendar.color" size="20px">$squareSolid</v-icon>
-      </v-list-item-icon>
       <v-list-item-content>
-        {{ calendar.name }}
+        <v-checkbox v-model="calendar.visibility" :color="calendar.color" :label="calendar.name"></v-checkbox>
       </v-list-item-content>
     </v-list-item>
   </v-list>
