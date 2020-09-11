a23907b27f03aed7cda4de50b1b0ee624a9cc5e7
youichiro <cinnamon416@gmail.com>
2020-09-11 21:54:46 +0900

    カレンダーのクリック処理

--- frontend/src/components/CalendarList.vue
+++ frontend/src/components/CalendarList.vue
@@ -1,14 +1,8 @@
 <template>
   <v-list dense>
-    <v-list-item v-for="(calendar, i) in calendars" :key="i">
+    <v-list-item v-for="(calendar, i) in calendars" :key="i" @click="toggleVisibility(calendar)">
       <v-list-item-content class="pa-0 ma-0">
-        <v-checkbox
-          dense
-          v-model="calendar.visibility"
-          :color="calendar.color"
-          :label="calendar.name"
-          class="ma-0 pa-0"
-        ></v-checkbox>
+        <v-checkbox dense v-model="calendar.visibility" :color="calendar.color" :label="calendar.name"></v-checkbox>
       </v-list-item-content>
     </v-list-item>
   </v-list>
@@ -26,7 +20,14 @@ export default {
     this.fetchCalendars();
   },
   methods: {
-    ...mapActions('calendars', ['fetchCalendars'])
+    ...mapActions('calendars', ['fetchCalendars', 'updateCalendar']),
+    toggleVisibility(calendar) {
+      const params = {
+        ...calendar,
+        visibility: !calendar.visibility
+      };
+      this.updateCalendar(params);
+    }
   }
 };
 </script>
