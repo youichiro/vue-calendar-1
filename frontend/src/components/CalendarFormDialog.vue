358fc94bdd6a8edee213265aabc3a057b6337a39
youichiro <cinnamon416@gmail.com>
2020-09-13 22:11:06 +0900

    カレンダーフォームの閉じボタンを表示

--- frontend/src/components/CalendarFormDialog.vue
+++ frontend/src/components/CalendarFormDialog.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>CalendarFormDialig</div>
+  <v-card light width="600">
+    <v-card-actions class="d-flex justify-end pa-2">
+      <v-btn icon @click="close">
+        <v-icon>$close</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'CalendarFormDialig'
+  name: 'CalendarFormDialig',
+  methods: {
+    ...mapActions('calendars', ['resetCalendar']),
+    close() {
+      this.resetCalendar();
+    }
+  }
 };
 </script>
