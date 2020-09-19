c1d743c52d8f775d82119978f10215dec1cf68cf
youichiro <cinnamon416@gmail.com>
2020-09-20 00:48:25 +0900

    date-fnsを使う

--- frontend/src/functions/datetime.js
+++ frontend/src/functions/datetime.js
@@ -1,33 +1,33 @@
-import moment from 'moment';
+import { format, addHours, addMinutes, isBefore } from 'date-fns';
 
 export const getCurrectDateStr = () => {
-  return moment().format('YYYY-MM-DD');
+  return format(new Date(), 'yyyy-MM-dd');
 };
 
 export const formatDateToMonthlyStr = date => {
-  return moment(date).format('YYYY年 M月');
+  return format(new Date(date), 'yyyy年 M月');
 };
 
 export const getDefaultStartAndEnd = date => {
-  const currentTime = moment().format('HH:mm:ss');
-  let datetime = moment(`${date} ${currentTime}`);
-  const start = `${datetime.add(1, 'h').format('YYYY-MM-DD HH')}:00:00`;
-  const end = `${datetime.add(1, 'h').format('YYYY-MM-DD HH')}:00:00`;
+  const currentTime = format(new Date(), 'HH:mm:ss');
+  const datetime = new Date(`${date} ${currentTime}`);
+  const start = format(addHours(datetime, 1), 'yyyy-MM-dd HH:00:00');
+  const end = format(addHours(datetime, 2), 'yyyy-MM-dd HH:00:00');
   return [start, end];
 };
 
-export const getTimeIntervalList = (intervalNum = 15, intervalUnit = 'minutes') => {
-  const start = moment('2020-08-01 00:00:00');
-  const end = moment('2020-08-01 24:00:00');
+export const getTimeIntervalList = () => {
+  const start = new Date('2020-08-01 00:00:00');
+  const end = new Date('2020-08-01 24:00:00');
   let times = [];
-  for (let target = start.clone(); target.isBefore(end); target.add(intervalNum, intervalUnit)) {
-    times.push(target.format('HH:mm'));
+  for (let target = new Date(start); isBefore(target, end); target = addMinutes(target, 15)) {
+    times.push(format(target, 'HH:mm'));
   }
   return times;
 };
 
 export const isGreaterEndThanStart = (startDate, startTime, endDate, endTime, allDay) => {
-  const start = moment(`${startDate} ${startTime}`);
-  const end = moment(`${endDate} ${endTime}`);
+  const start = new Date(`${startDate} ${startTime}`).getTime();
+  const end = new Date(`${endDate} ${endTime}`).getTime();
   return allDay ? end >= start : end > start;
 };
