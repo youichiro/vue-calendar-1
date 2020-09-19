28e3f02e078cf463a6516afe33c60a2fd9cb1af9
youichiro <cinnamon416@gmail.com>
2020-09-19 23:00:04 +0900

    api namespaceに変更

---
+++ backend/app/controllers/api/events_controller.rb
@@ -0,0 +1,41 @@
+module Api
+  class EventsController < ApplicationController
+    def index
+      render json: Event.all, each_serializer: EventSerializer
+    end
+
+    def show
+      render json: Event.find(params[:id]), serializer: EventSerializer
+    end
+
+    def create
+      event = Event.new(event_params)
+      if event.save
+        render json: event, serializer: EventSerializer
+      else
+        render json: event.errors, status: 422
+      end
+    end
+
+    def update
+      event = Event.find(params[:id])
+      if event.update(event_params)
+        render json: event, serializer: EventSerializer
+      else
+        render json: event.errors, status: 422
+      end
+    end
+
+    def destroy
+      event = Event.find(params[:id])
+      event.destroy!
+      render json: event, serializer: EventSerializer
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color, :calendar_id)
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :show, :create, :update, :destroy]
-  resources :calendars, only: [:index, :show, :create, :update, :destroy]
+  namespace 'api' do
+    resources :events, only: [:index, :show, :create, :update, :destroy]
+    resources :calendars, only: [:index, :show, :create, :update, :destroy]
+  end
 end
